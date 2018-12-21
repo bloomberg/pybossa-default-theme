ee3d0f013cb1de8e9031f3b69d2dee4f538f557d
Maria Rivera <mrivera144@bloomberg.net>
2018-12-21 16:28:56 -0500

    Initial commit component builder, need to refactor more and add test and finish text-box and checkbox

--- static/src/webpack.config.js
+++ static/src/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   output: {
     path: resolve('../js/gen'),
     publicPath: '/static/js/gen',
-    filename: 'editor.min.js'
+    filename: 'editor.min.[hash].js'
   },
 
   resolve: {
@@ -61,15 +61,21 @@ module.exports = {
     plugins: [
         new htmlWebpackPlugin({
           inject: false,
-          hash: true,
+          hash: false,
           filename: '../../../templates/projects/new_blogpost.html',
           template: '../../templates/projects/new_blogpost.webpack' 
         }),
         new htmlWebpackPlugin({
           inject: false,
-          hash: true,
+          hash: false,
           filename: '../../../templates/admin/new_announcement.html',
           template: '../../templates/admin/new_announcement.webpack'
+        }),
+        new htmlWebpackPlugin({
+          inject: false,
+          hash: false,
+          filename: '../../../templates/projects/componentBuilder.html',
+          template: '../../templates/projects/componentBuilder.webpack'
         }),
         new VueLoaderPlugin()
     ]
