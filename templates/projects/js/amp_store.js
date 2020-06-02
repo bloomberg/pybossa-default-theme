569a0286aa1e1513d4436af1f331918d48917444
Weiran Yuan <wyuan35@bloomberg.net>
2020-06-02 16:26:33 -0400

    add default-optin logic

--- templates/projects/js/amp_store.js
+++ templates/projects/js/amp_store.js
@@ -1,5 +1,11 @@
 <script type="text/javascript">
 $(function() {
+  // default opt-in for new projects
+  if (!$('#input_data_class').val() && !$('#output_data_class').val()){
+    optInAmpStore();
+  }
+
+  // opt-out L1 projects, users need to explicitly opt-in
   let dataClassification = 'L1';
   $('#input_data_class').change(function (e) {
     if ($(e.target).val().includes(dataClassification)) {
@@ -15,5 +21,9 @@ $(function() {
 
 function optOutAmpStore() {
   document.getElementById("amp_store").checked = false;
+}
+
+function optInAmpStore() {
+  document.getElementById("amp_store").checked = true;
 }
 </script>
