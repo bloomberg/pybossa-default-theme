4055759ae2973c484e2f26dae456f4782d6e6879
nsyed22 <nsyed22@bloomberg.net>
2023-01-24 14:14:01 -0500

    store guidelines state

--- templates/projects/js/annotation_guidelines_button.js
+++ templates/projects/js/annotation_guidelines_button.js
@@ -1,7 +1,19 @@
 <script>
-$(".collapse-guidelines-button").on('click', function(){
-    $(this).text(function(i, v){
-        return (v.startsWith('Show') ? 'Hide' : 'Show') + ' Instructions';
-    });
+const guidelines = $("#collapse-guidelines")
+const button = $(".collapse-guidelines-button")
+
+if (localStorage.getItem("guidelines-state") === "closed") {
+  guidelines.collapse("hide")
+  button.text('Show Instructions')
+}
+
+button.on('click', function(){
+        if (localStorage.getItem("guidelines-state") === "closed") {
+            localStorage.setItem("guidelines-state", "open")
+            button.text('Hide Instructions')
+        } else {
+            localStorage.setItem("guidelines-state", "closed")
+            button.text('Show Instructions')
+        }
 });
 </script>
