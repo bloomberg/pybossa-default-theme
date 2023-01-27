5ac8f6d2dbc7dd9d7e0d1b756540a134c4237eea
nsyed22 <nsyed22@bloomberg.net>
2023-01-27 11:44:54 -0500

    make state project specific

--- templates/projects/js/annotation_guidelines_button.js
+++ templates/projects/js/annotation_guidelines_button.js
@@ -1,18 +1,19 @@
 <script>
 const guidelines = $("#collapse-guidelines")
 const button = $(".collapse-guidelines-button")
+const key = '{{project.short_name}}' + "-guidelines-state"
 
-if (localStorage.getItem("guidelines-state") === "closed") {
+if (localStorage.getItem(key) === "closed") {
   guidelines.collapse("hide")
   button.text('Show Instructions')
 }
 
 button.on('click', function(){
-        if (localStorage.getItem("guidelines-state") === "closed") {
-            localStorage.setItem("guidelines-state", "open")
+        if (localStorage.getItem(key) === "closed") {
+            localStorage.setItem(key, "open")
             button.text('Hide Instructions')
         } else {
-            localStorage.setItem("guidelines-state", "closed")
+            localStorage.setItem(key, "closed")
             button.text('Show Instructions')
         }
 });
