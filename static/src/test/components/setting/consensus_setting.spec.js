c4e91c3769d0b70034a7fb4f3b388ea01d6e1957
Noor Syed <noorsyed1a@gmail.com>
2024-05-08 16:17:25 -0400

    update tests

--- static/src/test/components/setting/consensus_setting.spec.js
+++ static/src/test/components/setting/consensus_setting.spec.js
@@ -1,67 +1,25 @@
 import Vuex from 'vuex';
 import { createLocalVue, shallowMount } from '@vue/test-utils';
-import RedundancyConfig from '../../../components/fieldsconfig/redundancy_config.vue';
+import ConsensusConfig from '../../../components/setting/consensus_setting.vue';
 import { storeSpecs } from '../../../components/fieldsconfig/store';
 import { cloneDeep } from 'lodash';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
-describe('RedundancyConfig', () => {
+describe('ConsensusConfig', () => {
   let store;
   let fetch;
   let notify;
   let consensusConfig = {
-    consensus_threshold: 80,
-    redundancy_config: 2,
-    max_retries: 10
+    consensus_method: "dice",
+    agreement_threshold: 80
   };
 
   beforeEach(() => {
     store = new Vuex.Store(cloneDeep(storeSpecs));
     fetch = global.fetch = jest.fn();
     notify = window.pybossaNotify = jest.fn();
-  });
-
-  it('fetch data', async () => {
-    let response = {
-      consensus_config: JSON.stringify(consensusConfig),
-      answer_fields: {
-        testField: {
-          type: 'categorical',
-          config: {
-            labels: ['A', 'B', 'C']
-          },
-          retryForConsensus: false
-        }
-      }
-    };
-    fetch.mockImplementation((arg) => ({
-      ok: true,
-      json: () => Promise.resolve(response)
-    }));
-    const wrapper = shallowMount(RedundancyConfig, { store, localVue });
-    await localVue.nextTick();
-    expect(wrapper.vm._data.consensusThreshold).toBe(80);
-    expect(wrapper.vm._data.redundancyConfig).toBe(2);
-    expect(wrapper.vm._data.maxRetries).toBe(10);
-  });
-
-  it('does not load data', () => {
-    store.commit('setData', {
-      answerFields: {
-        testField: {
-          type: 'categorical',
-          config: {
-            labels: ['A', 'B', 'C']
-          },
-          retryForConsensus: false
-        }
-      }
-    });
-    const wrapper = shallowMount(RedundancyConfig, { store, localVue });
-    const p = wrapper.findAll('p');
-    expect(p).toHaveLength(0);
   });
 
   it('loads empty config', () => {
@@ -76,9 +34,9 @@ describe('RedundancyConfig', () => {
         }
       }
     });
-    const wrapper = shallowMount(RedundancyConfig, { store, localVue });
+    const wrapper = shallowMount(ConsensusConfig, { store, localVue });
     const p = wrapper.findAll('p');
-    expect(p).toHaveLength(3);
+    expect(p).toHaveLength(1);
   });
 
   it('load non-empty config', () => {
@@ -94,11 +52,11 @@ describe('RedundancyConfig', () => {
       },
       consensus: consensusConfig
     });
-    const wrapper = shallowMount(RedundancyConfig, { store, localVue });
+    const wrapper = shallowMount(ConsensusConfig, { store, localVue });
     const p = wrapper.findAll('p');
     const button = wrapper.findAll('button');
     expect(button).toHaveLength(1);
-    expect(p).toHaveLength(3);
+    expect(p).toHaveLength(1);
   });
 
   it('save config (without consensus config)', async () => {
@@ -117,7 +75,7 @@ describe('RedundancyConfig', () => {
         }
       }
     });
-    const wrapper = shallowMount(RedundancyConfig, { store, localVue });
+    const wrapper = shallowMount(ConsensusConfig, { store, localVue });
     const saveButton = wrapper.findAll('button').at(0);
     saveButton.trigger('click');
     await localVue.nextTick();
@@ -142,36 +100,12 @@ describe('RedundancyConfig', () => {
       },
       consensus: consensusConfig
     });
-    const wrapper = shallowMount(RedundancyConfig, { store, localVue });
+    const wrapper = shallowMount(ConsensusConfig, { store, localVue });
     const saveButton = wrapper.findAll('button').at(0);
     saveButton.trigger('click');
     await localVue.nextTick();
     expect(fetch.mock.calls).toHaveLength(2);
     expect(notify.mock.calls).toHaveLength(2);
-  });
-
-  it('save incorrect config', () => {
-    store.commit('setData', {
-      answerFields: {
-        testField: {
-          type: 'categorical',
-          config: {
-            labels: ['A', 'B', 'C']
-          },
-          retry_for_consensus: true
-        }
-      },
-      consensus: {
-        consensus_threshold: 80,
-        max_retries: 8,
-        redundancy_config: 'wrong'
-      }
-    });
-    const wrapper = shallowMount(RedundancyConfig, { store, localVue });
-    expect(wrapper.findAll('.error-msg')).toHaveLength(0);
-    const saveButton = wrapper.findAll('button').at(0);
-    saveButton.trigger('click');
-    expect(wrapper.findAll('.error-msg')).toHaveLength(1);
   });
 
   it('save config fails', async () => {
@@ -189,25 +123,11 @@ describe('RedundancyConfig', () => {
         }
       }
     });
-    const wrapper = shallowMount(RedundancyConfig, { store, localVue });
+    const wrapper = shallowMount(ConsensusConfig, { store, localVue });
     const saveButton = wrapper.findAll('button').at(0);
     saveButton.trigger('click');
     await localVue.nextTick();
     expect(fetch.mock.calls).toHaveLength(2);
     expect(notify.mock.calls).toHaveLength(2);
-  });
-
-  it('test validation logic directly', () => {
-    const wrapper = shallowMount(RedundancyConfig, { store, localVue });
-    let valid = wrapper.vm._write(80, 2, 10, 70); // should pass
-    expect(valid).toBe(true);
-
-    valid = wrapper.vm._write(101, 2, 10, 70); // threshold out of range
-    expect(valid).toBe(false);
-    expect(wrapper.vm.errorMsg).toContain('within 50 - 100');
-
-    valid = wrapper.vm._write(80, -1, 10, 70); // redundancy should be positive
-    expect(valid).toBe(false);
-    expect(wrapper.vm.errorMsg).toContain('positive integer');
   });
 });
